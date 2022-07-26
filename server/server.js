3001ffacc6751a9cb4534ab1e5d7ed091f3855e1
Hafiz Hamza Ali <HamzaAli.900900@gmail.com>
2022-07-26 19:04:43 +0500

    product event with api integration

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const request = require('request')
 exports = {
 
   /**
@@ -35,5 +36,52 @@ exports = {
   onAppUninstallCallback: function (payload) {
     console.log("Logging arguments from onAppUninstall event: " + JSON.stringify(payload));
     renderData();
+  },
+
+  // implementation of product event  
+  /**
+   * @param {Object} payload
+   */
+  onUserCreateCallback: function (payload) {
+    let opt = {
+      method:"POST",
+      url : "http://localhost:3000",
+      headers: {
+      // "Authorization": "Bearer <%= iparam.apiKey %>",
+      "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+      }
+      
+      request(opt , function(err , res , body){
+      const response = JSON.stringify(body);
+      console.log(response);
+      })
+      renderData();
+  },
+  /**
+   * @param {Object} payload
+   */
+  onUserUpdateCallback: function (payload) {
+    console.log(payload);
+  },
+  /**
+   * @param {Object} payload
+   */
+  onMessageCreateCallback: function (payload) {
+    console.log(payload);
+  },
+  /**
+   * @param {Object} payload
+   */
+  onConversationUpdateCallback: function (payload) {
+    console.log(payload);
+  },
+  /**
+   * @param {Object} payload
+   */
+  onConversationCreateCallback: function (payload) {
+    console.log(payload);
   }
 }
+
